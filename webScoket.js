c47e22ba91da4516d172c5599aaec45a9afc42d4
pedro.teles <pedro.teles@arcom.com.br>
2023-05-22 17:18:27 -0300

    att

--- webScoket.js
+++ webScoket.js
@@ -14,7 +14,7 @@ const server = http.createServer(app)
 const wsServer = new WebSocketServer({ httpServer: server })
 
 // Armazena todas as conexões ativas
-const connections = []
+
 
 db.run(`CREATE TABLE IF NOT EXISTS mensagens (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -23,25 +23,27 @@ db.run(`CREATE TABLE IF NOT EXISTS mensagens (
   enviado INTEGER
 )`)
 
+const connections = []
 wsServer.on('request', (request) => {
-  const connectionParams = request.resourceURL
   const connection = request.accept(null, request.origin)
-  connection.idUsuario = connectionParams.query?.idUsuario
-  connections.push(connection) // Adiciona a nova conexão à lista
-  verificarConexoesAtivas(connection.idUsuario)
-  // console.log('Nova conexão estabelecida.')
   connection.on('message', async (message) => {
     if (message.type === 'utf8') {
       try {
         const receivedMessage = JSON.parse(message.utf8Data)
-        // Insira a mensagem na tabela 'mensagens' somente se o usuário estiver offline
-        const targetConnection = connections.find((conn) => conn.idUsuario === receivedMessage.idMatricula)
-        if (!targetConnection || !targetConnection.connected) {
-          const insertQuery = 'INSERT INTO mensagens (idMatricula, mensagem, enviado) VALUES (?, ?, ?)'
-          await runQuery(insertQuery, [receivedMessage.idMatricula, receivedMessage.mensagem, 0])
-          console.log('Mensagem inserida com sucesso:', this.lastID)
+        if (receivedMessage?.cadastro) {
+          console.log('Nova conexão estabelecida.' + receivedMessage.idMatricula)
+          connection.idUsuario = receivedMessage.idMatricula
+          connections.push(connection) // Adiciona a nova conexão à lista
+          verificarConexoesAtivas(connection.idUsuario)
         } else {
-          targetConnection.sendUTF(JSON.stringify({ idMatricula: receivedMessage.idMatricula, mensagem: receivedMessage.mensagem }))
+          // Insira a mensagem na tabela 'mensagens' somente se o usuário estiver offline
+          const targetConnection = connections.find((conn) => conn.idUsuario === receivedMessage.idMatricula)
+          if (!targetConnection || !targetConnection.connected) {
+            const insertQuery = 'INSERT INTO mensagens (idMatricula, mensagem, enviado) VALUES (?, ?, ?)'
+            await runQuery(insertQuery, [receivedMessage.idMatricula, receivedMessage.mensagem, 0])
+          } else {
+            targetConnection.sendUTF(JSON.stringify({ idMatricula: receivedMessage.idMatricula, mensagem: receivedMessage.mensagem }))
+          }
         }
       } catch (error) {
         console.error('Erro ao analisar a mensagem JSON:', error)
@@ -53,7 +55,6 @@ wsServer.on('request', (request) => {
   // Remove a conexão da lista de conexões ativas
   connection.on('close', () => {
     const index = connections.indexOf(connection)
-
     // Remover mensagens do banco de dados associadas ao usuário
     const deleteQuery = `DELETE FROM mensagens WHERE idMatricula = ?`
     runQuery(deleteQuery, [connection.idUsuario], (err, result) => {
@@ -63,12 +64,10 @@ wsServer.on('request', (request) => {
         console.log('Mensagens removidas com sucesso:', result.affectedRows)
       }
     })
-
     if (index !== -1) {
       connections.splice(index, 1)
     }
   })
-
 })
 
 process.on('exit', () => {
@@ -95,7 +94,6 @@ function verificarConexoesAtivas(idUsuario) {
     if (err) {
       console.error('Erro ao consultar mensagens:', err)
     } else {
-      console.log(rows, idUsuario)
       rows.forEach((row) => {
         const idMatricula = row.idMatricula
         const mensagem = row.mensagem
@@ -113,14 +111,20 @@ function verificarConexoesAtivas(idUsuario) {
   })
 }
 
-app.post('/enviar-mensagem', (req, res) => {
-  const message = {
+app.post('/enviar-mensagem', async (req, res) => {
+  const receivedMessage = {
     idMatricula: req.body.idMatricula,
     mensagem: req.body.mensagem
   }
-  connections.forEach((connection) => {
-    connection.sendUTF(JSON.stringify(message))
-  })
+  const targetConnection = connections.find((conn) => conn.idUsuario === receivedMessage.idMatricula)
+
+  if (!targetConnection || !targetConnection.connected) {
+    const insertQuery = 'INSERT INTO mensagens (idMatricula, mensagem, enviado) VALUES (?, ?, ?)'
+    await runQuery(insertQuery, [receivedMessage.idMatricula, receivedMessage.mensagem, 0])
+  } else {
+    targetConnection.sendUTF(JSON.stringify({ idMatricula: receivedMessage.idMatricula, mensagem: receivedMessage.mensagem }))
+  }
+
   res.json({ status: 'success', message: 'Mensagem enviada com sucesso.' })
 })
 
